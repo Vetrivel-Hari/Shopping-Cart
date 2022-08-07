a550130c6524f3a25881ef4d64583818f5cda689
Vetrivel M.D <44906792+Vetrivel-Hari@users.noreply.github.com>
2022-08-08 01:54:47 +0530

    Add files via upload

---
+++ assets/js/shop.js
@@ -0,0 +1,78 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        item_data: [],
+        item_url:[]
+    },
+    methods: {
+            addItemToCart(){
+                let selected_index = event.srcElement.id;
+                let c = 0;
+                
+                
+                cook = document.cookie;
+                cook = cook.slice(cook.indexOf("{"))
+
+                let cartCookies = []
+                let jsonCartCookies = []
+
+                if(cook != ''){
+                    cartCookies = JSON.parse(cook.slice(cook.indexOf("{")))
+                    
+                    for(i of cartCookies['data']){
+                        if(i['_id'] == this.item_data[selected_index]['_id']){
+                            i['quantity'] += 1
+                            c = 1;
+                            break;
+                        }
+                        console.log(i)
+                    }
+                }
+
+                if(c == 0) {
+                    let t = this.item_data[selected_index]
+                    t['url'] = this.item_url[selected_index]
+                    t['quantity'] = 1
+
+                    if(cartCookies.length == 0){
+                        cartCookies.push(t);
+                        jsonCartCookies = {"data" : cartCookies}
+                    }
+                    else{
+                        cartCookies['data'].push(t);
+                        jsonCartCookies = {"data" : cartCookies['data']}
+                    }
+                    
+                        
+                }
+                else{
+                    jsonCartCookies = {"data" : cartCookies['data']}
+                }
+
+
+                document.cookie = 'cart=' + JSON.stringify(jsonCartCookies) + ';max-age=31536000;';
+                console.log(document.cookie);
+            },
+    },
+    async beforeMount()
+    {
+        let requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          await fetch("https://webenggshopcart.herokuapp.com/", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                for(i of result['data']['data']){
+                    console.log(i['name'])
+                    this.item_data.push(i);
+                }
+            })
+            .catch(error => console.log('error', error));
+
+            for (i of this.item_data){
+                this.item_url.push(i['url'])
+            }
+    }
+})
